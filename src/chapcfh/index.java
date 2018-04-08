3dfd7ad4fbc496a3cfe37ae61b45d78a4b15f225
AkshayC <akshay35chougule@gmail.com>
2018-04-08 10:47:34 +0530

    new class added

---
+++ src/chapcfh/index.java
@@ -0,0 +1,12 @@
+package chapcfh;
+
+public class index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("javaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+System.out.println("javaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+System.out.println("javaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+	}
+
+}
