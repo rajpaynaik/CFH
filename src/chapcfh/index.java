81761e2d618787429b675eb873d0bbf2f80634e4
AkshayC <akshay35chougule@gmail.com>
2018-04-08 10:52:47 +0530

    add

--- src/chapcfh/index.java
+++ src/chapcfh/index.java
@@ -5,5 +5,4 @@ public class index {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("javaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-System.out.println("javaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 	}}
